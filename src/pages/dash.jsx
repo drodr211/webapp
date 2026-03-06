b9827f28c590c32a4f22a4588aad67fa27eca720
drodr211 <drodr211@ucr.edu>
2026-03-06 09:33:48 -0800

    dash (no wrapper)

--- src/pages/dash.jsx
+++ src/pages/dash.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import supabase  from "../helper/supabaseClient"
+import { Link, useNavigate} from "react-router-dom";
 
 function Dashboard() {
-    return <div>Dashboard</div>;
+    const navigate = useNavigate();
+
+    const signOut = async () => {
+        const { error } = await supabase.auth.signOut();
+        if (error) throw error;
+        navigate("/login");
+    }
+    return (
+        <div>
+            <h2>Welcome in.</h2>
+            <button onClick={signOut}>Sign Out</button>
+
+        </div>
+    );
 }
 
 export default Dashboard;
