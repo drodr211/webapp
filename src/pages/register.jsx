3dcf927b3779c3d4d1217c68ae5cb2ad7e844066
drodr211 <drodr211@ucr.edu>
2026-03-06 08:41:12 -0800

    user creation works

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import supabase from "../helper/supabaseClient"
+import supabase  from "../helper/supabaseClient"
 import "../helper/register.css"
 
 function Register() {
@@ -11,33 +11,38 @@ function Register() {
         event.preventDefault();
         setMessage("");
 
-        const {data, error} = await supabase.auth.signup({
-            email: email, password: password,
+        const { data, error } = await supabase.auth.signUp({
+            email: email, 
+            password: password,
         });
 
-        if (error) { setMessage(error.setMessage); return; }
-
+        if (error) { setMessage(error.message); return; }
         if (data) { setMessage("User Account Created"); }
+
+        setEmail(""); setPassword("");
 
     };
 
     return (
         <div className="maxSize centercenter">
             <h2 >Register</h2>
+            <br></br>
             {message && <span>{message}</span>}
             <form className="centercenter" onSubmit={handleSubmit}>
-                <p>Username:</p>
-                <input type="text" placeholder="Email" required 
+                <p>Email:</p>
+                <input type="email" placeholder="Email" required 
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 ></input>
+                
                 <p>Password:</p>
                 <input type="password" placeholder="Password" required
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 ></input>
+                
                 <br></br>
-                <button type="Submit">Create Account</button>
+                <button type="submit">Create Account</button>
             </form> 
         </div>
     );
